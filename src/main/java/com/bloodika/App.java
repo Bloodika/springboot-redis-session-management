05ff2d934089ca80c597ce88dd521ea138d8c39a
bloodika <bloodika@gmail.com>
2023-01-06 21:01:22 +0100

    init

---
+++ src/main/java/com/bloodika/App.java
@@ -0,0 +1,9 @@
+package com.bloodika;
+import org.springframework.boot.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        System.out.println("He");
+    }
+}
